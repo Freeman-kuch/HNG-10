5b59f0006412b3af88a0f345fd7d520f331705ec
Freeman <freemanoparaocha@gmail.com>
2022-11-02 12:26:28 +0100

    modified requirements

--- test/test_app.py
+++ test/test_app.py
@@ -2,7 +2,7 @@ import unittest
 import requests
 
 class Bio_test(unittest.TestCase):
-    def Bio(self):
-        endpoint = requests.get("")
+    def test_Bio(self):
+        endpoint = requests.get("https://hng9-bio.herokuapp.com/")
         assert endpoint.status_code == 200
         assert endpoint.text == "OK"
