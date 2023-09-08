d703f645234e0e331802d91ee8b59c0bee5edfa4
Freeman <freemanoparaocha@gmail.com>
2023-09-08 11:31:21 +0100

    test

--- test/test_app.py
+++ test/test_app.py
@@ -3,6 +3,7 @@ import requests
 
 class Bio_test(unittest.TestCase):
     def test_Bio(self):
-        endpoint = requests.get("https://hng9-bio.herokuapp.com/")
+        endpoint = requests.get("http://127.0.0.1:5000/bio?slack_name=freeman&track=backend")  
+        # the url should be changed once deployed to live servere
         assert endpoint.status_code == 200
         assert endpoint.text == "OK"
