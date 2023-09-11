6e59b96a915083cf708e9bf67c9d6695ab4303e0
Freeman-kuch <freemanoparaoch@gmail.com>
2023-09-11 23:10:34 +0100

    render>>

--- app.py
+++ app.py
@@ -5,19 +5,23 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
-def create_app():
 
-    app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("SQL_THINGY")
-    # app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-    api = Api(app)
-
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("SQL_THINGY")
+# app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+api = Api(app)
 
 
-    api.add_resource(bio, "/api")
-    api.add_resource(stage_2, "/api/<int:user_id>/")
 
-    return app
+api.add_resource(bio, "/api")
+api.add_resource(stage_2, "/api/<int:user_id>/")
 
+
+if __name__ == "__main__":
+    from db import db
+    db.init_app(app)
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
 
 
--- wsgi.py
+++ wsgi.py
@@ -1,14 +1,5 @@
-from app import create_app
-from db import db  
-
-app = create_app()
-db.init_app(app)
-with app.app_context():
-    db.create_all()
+from app import app
 
 
 if __name__ == "__main__":
-    # db.init_app(app)
-    # with app.app_context():
-    #     db.create_all()
     app.run(debug=True)
