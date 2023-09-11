6261d18d2f7cec465d3fd5562c67e7d15595d134
Freeman-kuch <freemanoparaoch@gmail.com>
2023-09-11 23:05:43 +0100

    render>>

--- wsgi.py
+++ wsgi.py
@@ -2,11 +2,13 @@ from app import create_app
 from db import db  
 
 app = create_app()
+db.init_app(app)
+with app.app_context():
+    db.create_all()
 
 
 if __name__ == "__main__":
-    from db import db
-    db.init_app(app)
-    with app.app_context():
-        db.create_all()
+    # db.init_app(app)
+    # with app.app_context():
+    #     db.create_all()
     app.run(debug=True)
