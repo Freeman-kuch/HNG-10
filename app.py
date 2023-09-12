22e1e0c41ad956ffa93b698f289abe2e5cbb81e1
Freeman-kuch <freemanoparaoch@gmail.com>
2023-09-12 01:52:12 +0100

    final commit

--- app.py
+++ app.py
@@ -3,13 +3,17 @@ from flask_restful import Api
 from resource.endpoints import bio, stage_2
 from dotenv import load_dotenv
 import os
+from db import db
 
 load_dotenv()
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("SQL_THINGY")
-# app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 api = Api(app)
+db.init_app(app)
+with app.app_context():
+    db.create_all()
 
 
 
@@ -18,10 +22,6 @@ api.add_resource(stage_2, "/api/<int:user_id>/")
 
 
 if __name__ == "__main__":
-    from db import db
-    db.init_app(app)
-    with app.app_context():
-        db.create_all()
     app.run(debug=True)
 
 
